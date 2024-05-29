62c5e49454918191a2f105b5cce482997967db65
manupanjoria <manu.panjoria@bonamisoftware.com>
2024-05-29 14:57:44 +0530

    backend commit 1

---
+++ connection.js
@@ -0,0 +1,41 @@
+const { Client } = require('pg');
+
+// const client = new Client( {
+//   host:"localhost",
+//   user: "postgres",
+//   port: 5433,
+//   password: "Saurav@132",
+//   database: "IRIS"
+// });
+
+// const client = new Client( {
+//   host:"localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "iru@ADMIN123",
+//   database: "iru_imd_db"
+// });
+// const client = new Client( {
+//   host:"localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "123456",
+//   database: "IRIS"
+// });
+
+const client = new Client( {
+  host:"localhost",
+  user: "postgres",
+  port: 5432,
+  password: "12345",
+  database: "iris"
+});
+// const client = new Client( {
+//   host:"localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "123456",
+//   database: "IRIS"
+// });
+
+module.exports = client;

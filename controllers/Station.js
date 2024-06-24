9f8960b186be77005e46ee49e9c68b47cc74eabd
ghanshyam-pal <ghanshyampal789@gmail.com>
2024-06-24 14:48:48 +0530

    added fetch station data api

---
+++ controllers/Station.js
@@ -0,0 +1,70 @@
+
+const express = require("express");
+const router = express.Router();
+const app = express();
+const client = require("../connection");
+const moment = require('moment');
+
+
+exports.fetchStationData = async (req, res) => {
+    try {
+        let { Date } = req.body;
+
+        // Use current date if no dates are provided
+        const currentDate = moment().format('YYYY-MM-DD');
+        if (!Date ) {
+            Date =  currentDate;
+        } 
+
+        let data = await fetchFilteredData(Date);
+
+        res.status(200).json({
+            success: true,
+            message: "Station data fetched Successfully",
+            data: data
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch Station data",
+            error: error.message,
+        });
+    }
+}
+
+const fetchFilteredData = async (Date) => {
+    const query = `
+                SELECT ndd.region_code,
+                    ndd.region_name,
+                    ndd.new_state_code,
+                    ndd.state_name,
+                    ndd.district_code,
+                    ndd.district_name,
+                    sd.station_code,
+                    sd.station_name,
+                    sd.station_type,
+                    sd.centre_type,
+                    sd.centre_name,
+                    sd.is_new_station,
+                    sd.latitude,
+                    sd.longitude,
+                    sd.activationdate,
+                    sdd.data
+                FROM public.station_details AS sd
+                JOIN public.station_daily_data AS sdd 
+                    ON sdd.station_id = sd.station_code
+                JOIN normal_district_details AS ndd 
+                    ON ndd.district_code = sdd.district_code
+                WHERE sdd.collection_date = $1`;
+
+    try {
+        const result = await client.query(query, [Date]);
+        return result.rows;
+    } catch (error) {
+        console.error('Error executing query', error.stack);
+        throw error;
+    }
+}
+
--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { insertStationData} = require("../controllers/scripts/station/stationDailyData")
 const { createStationDetailsTable } = require("../controllers/scripts/station/station_details")
+const { fetchStationData } = require("../controllers/Station")
 
 
 // ********************************************************************************************************
@@ -9,5 +10,6 @@ const { createStationDetailsTable } = require("../controllers/scripts/station/st
 // ********************************************************************************************************
 router.get("/insertStationData", insertStationData);
 router.get("/createStationDetailsTable", createStationDetailsTable);
+router.post("/fetchStationData", fetchStationData);
 
 module.exports = router;

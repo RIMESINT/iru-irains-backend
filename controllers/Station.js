99f04fdc73050bcc86c35066f25982127d72a4b1
ghanshyam-pal <ghanshyampal789@gmail.com>
2024-06-25 16:55:45 +0530

    added update station api

--- controllers/Station.js
+++ controllers/Station.js
@@ -34,6 +34,38 @@ exports.fetchStationData = async (req, res) => {
     }
 }
 
+
+exports.updateStationData = async (req, res) => {
+    try {
+        let { station_code, date, value } = req.body;
+        if(station_code && date && (value||value==0) ){
+            let data = await updateStationDataQuery(station_code, date, value );
+
+            res.status(200).json({
+                success: true,
+                message: " data updated Successfully",
+            });
+
+        }else{
+            res.status(200).json({
+                success: false,
+                message: "request pearmeters are missing",
+            });
+        }
+
+
+
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch Station data",
+            error: error.message,
+        });
+    }
+}
+
 const fetchFilteredData = async (Date) => {
     const query = `
                 SELECT ndd.region_code,
@@ -61,6 +93,20 @@ const fetchFilteredData = async (Date) => {
 
     try {
         const result = await client.query(query, [Date]);
+        return result.rows;
+    } catch (error) {
+        console.error('Error executing query', error.stack);
+        throw error;
+    }
+}
+
+const updateStationDataQuery = async (station_code, date, value ) => {
+    const query = `
+                Update public.station_daily_data set data =$1
+                WHERE collection_date = $2 and station_id = $3;`;  
+
+    try {
+        const result = await client.query(query, [value, date,station_code ]);
         return result.rows;
     } catch (error) {
         console.error('Error executing query', error.stack);
--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { insertStationData} = require("../controllers/scripts/station/stationDailyData")
 const { createStationDetailsTable } = require("../controllers/scripts/station/station_details")
-const { fetchStationData } = require("../controllers/Station")
+const { fetchStationData ,updateStationData} = require("../controllers/Station")
 
 
 // ********************************************************************************************************
@@ -11,5 +11,7 @@ const { fetchStationData } = require("../controllers/Station")
 router.get("/insertStationData", insertStationData);
 router.get("/createStationDetailsTable", createStationDetailsTable);
 router.post("/fetchStationData", fetchStationData);
+router.post("/updateStationData", updateStationData);
+updateStationData
 
 module.exports = router;

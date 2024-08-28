982c088d1ba10b0e0bbd5f1596e2cf4e67c678fa
ghanshyam-pal <ghanshyampal789@gmail.com>
2024-08-28 14:56:54 +0530

    change cron time

--- controllers/CronJobs.js
+++ controllers/CronJobs.js
@@ -1,8 +1,9 @@
 const schedule = require('node-schedule');
 const { AddDailyStationData} = require("../controllers/StationDataUpdates")
 const { dailyDataUpdateReminder, dailyDataVerificationReminder} = require("../controllers/Email")
-// Schedule a job to run at 2:00 PM every day
+// Schedule a job to run at 1:29 PM every day
 const job1 = schedule.scheduleJob('59 7 * * *', AddDailyStationData);
 //12:30
 const job2 = schedule.scheduleJob('1 7 * * *', dailyDataUpdateReminder);
+//1:15
 const job3 = schedule.scheduleJob('45 7 * * *', dailyDataVerificationReminder);

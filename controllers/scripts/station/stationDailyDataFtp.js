23343f978da2f67f753fb8658dfd98cb307c7a2a
BALU2000-creator <20pa1a0504@vishnu.edu.in>
2025-01-15 11:57:17 +0530

    removge truncate

--- controllers/scripts/station/stationDailyDataFtp.js
+++ controllers/scripts/station/stationDailyDataFtp.js
@@ -65,19 +65,14 @@ exports.insertStationDataFtp = async(req, res) => {
 
     // const queryText = "INSERT INTO station_daily_data_ftp (district_code ,station_id, collection_date, data) VALUES ";
     const queryText = `
-          INSERT INTO station_daily_data_ftp (district_code, station_id, collection_date, data) 
-          VALUES ($1, $2, $3, $4)
-          ON CONFLICT (station_id, collection_date, district_code) 
-          DO UPDATE SET 
-            data = EXCLUDED.data;
-          `;
-    // const queryText1 = "INSERT INTO station_daily_data (district_code ,station_id, collection_date, data) VALUES ";
-    // const truncateQuery = "TRUNCATE TABLE  public.station_daily_data_ftp";
-    // const truncateQuery1 = "TRUNCATE TABLE  public.station_daily_data";
-    const new_query = queryText + result.join(", ") + ";";
-    // const new_query1 = queryText1 + result.join(", ") + ";";
-    // await client.query(truncateQuery);
-    // await client.query(truncateQuery1);
+    INSERT INTO station_daily_data_ftp (district_code, station_id, collection_date, data) 
+    VALUES `;
+    
+    const new_query = queryText + result.join(", ") + `
+    ON CONFLICT (station_id, collection_date, district_code) 
+    DO UPDATE SET 
+      data = EXCLUDED.data;
+    `;
     await client.query(new_query);
     // await client.query(new_query1);
 

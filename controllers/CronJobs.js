d752cdcdab3eb1882e1452c31d2bdb2538f6215c
BALU2000-creator <20pa1a0504@vishnu.edu.in>
2025-07-01 11:09:14 +0530

    4:30PM cronjob

--- controllers/CronJobs.js
+++ controllers/CronJobs.js
@@ -7,3 +7,6 @@ const job1 = schedule.scheduleJob('29 13 * * *', AddDailyStationData);
 const job2 = schedule.scheduleJob('30 12 * * *', dailyDataUpdateReminder);
 //1:15
 const job3 = schedule.scheduleJob('15 13 * * *', dailyDataVerificationReminder);
+
+//4:29PM
+const job4 = schedule.scheduleJob('29 16 * * *', AddDailyStationData);

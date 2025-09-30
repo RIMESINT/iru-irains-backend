2d9459b159d6f1ad318e6cfdbcb7c72ccb984828
BALU2000-creator <20pa1a0504@vishnu.edu.in>
2025-09-30 11:00:49 +0530

    cronjob

--- controllers/CronJobs.js
+++ controllers/CronJobs.js
@@ -2,6 +2,7 @@ const schedule = require('node-schedule');
 const { AddDailyStationData} = require("../controllers/StationDataUpdates")
 const { dailyDataUpdateReminder, dailyDataVerificationReminder} = require("../controllers/Email");
 const { sendBulkReports } = require('./emailController');
+const { aggregateCurrentSeasonData } = require('./scripts/all/calculateAndIncludeAllDatesData'); // Adjust path as needed
 
 // Schedule jobs to run every 30 minutes from 1:29 PM to 6:00 PM
 const cronPatterns = [
@@ -22,6 +23,7 @@ const jobs = cronPatterns.map((pattern, index) =>
 );
 
 // Other existing jobs
+// const seasonalJobs = schedule.scheduleJob('05 18 * * *',  aggregateCurrentSeasonData)
 const job2 = schedule.scheduleJob('30 12 * * *', dailyDataUpdateReminder); // 12:30 PM
 const job3 = schedule.scheduleJob('15 13 * * *', dailyDataVerificationReminder); // 1:15 PM
 const job4 = schedule.scheduleJob('59 14 * * *', sendBulkReports); // 14:59 ~~ 3:00PM

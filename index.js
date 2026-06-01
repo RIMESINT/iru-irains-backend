ff459068cb36a38aeaf9cd560c712289c7ee68aa
builderbalu12-eng <builderbalu12@gmail.com>
2026-06-01 13:42:55 +0530

    calauion mode changed

--- index.js
+++ index.js
@@ -28,13 +28,14 @@ const awsRoutes = require("./routes/awsRoutes");
 const adminRoutes = require("./routes/adminpanelRoutes");
 const geojsonRoutes = require("./routes/geojsonRoutes");
 const stationDashboardRoutes = require("./routes/stationDashboardRoutes");
+const calculationsModeRoutes = require("./routes/calculationsModeRoutes");
 
 // // Load SSL Certificate
-const options = {
-    key: fs.readFileSync("/etc/apache2/private.key"),
-    cert: fs.readFileSync("/etc/apache2/*.imd.gov.in.crt"),
-    ca: fs.readFileSync("/etc/apache2/emSign-SSL-CA-G1.crt"),
-};
+// const options = {
+//     key: fs.readFileSync("/etc/apache2/private.key"),
+//     cert: fs.readFileSync("/etc/apache2/*.imd.gov.in.crt"),
+//     ca: fs.readFileSync("/etc/apache2/emSign-SSL-CA-G1.crt"),
+// };
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
@@ -107,6 +108,7 @@ app.use("/api/v1/", awsRoutes);
 app.use("/api/v1/", adminRoutes);
 app.use("/api/v1/", geojsonRoutes);
 app.use("/api/v1/", stationDashboardRoutes);
+app.use("/api/v1/", calculationsModeRoutes);
 
 // Use HTTPS Server
 https.createServer(options, app).listen(port, () => {
